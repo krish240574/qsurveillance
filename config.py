20ec33c317d8369917102c805640c3b291a35444
Kumar <kumar.ramanathan@gmail.com>
2022-07-15 03:13:24 -0700

    initial commit

---
+++ config.py
@@ -0,0 +1,18 @@
+import logging 
+
+
+log_level = logging.INFO
+timeout = 1
+kafka_host = "localhost:9092"
+
+# tweets 
+key = "FraudIEEE"
+topic = "CryptoData"
+csv_file = "train.csv"
+log_file = "log_tickets.log"
+
+# survey 
+key1 = "CryptoData"
+topic1 = "CryptoData"
+csv_file1 = "crypto.csv"
+log_file1 = "CryptoData.log"
